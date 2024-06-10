d84003ab28d47d2f92e1c37a1cc57389da136115
Hyeju Kim <kimoju03@naver.com>
2024-06-10 16:43:49 +0900

    fix contextLoads() FAILED build error

--- src/test/java/org/example/week14_todoapp/Week14TodoappApplicationTests.java
+++ src/test/java/org/example/week14_todoapp/Week14TodoappApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.week14_todoapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Week14TodoappApplicationTests.class)
 class Week14TodoappApplicationTests {
 
     @Test
