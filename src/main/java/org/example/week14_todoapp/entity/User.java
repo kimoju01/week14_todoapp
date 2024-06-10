9a2ad07191effa0e50be2a6826c486471aaaa479
Hyeju Kim <kimoju03@naver.com>
2024-06-10 16:29:12 +0900

    Initial commit

---
+++ src/main/java/org/example/week14_todoapp/entity/User.java
@@ -0,0 +1,18 @@
+package org.example.week14_todoapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
---
+++ src/test/java/org/example/week14_todoapp/Week14TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.week14_todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week14TodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
